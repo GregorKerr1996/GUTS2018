38adaa9e2176f46df452257b540e103379224def
SamJGeo <sam_g.1@hotmail.co.uk>
2018-10-13 05:01:52 +0100

    Hive bot

---
+++ HIVE Bot/HIVEBot.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python
+
+import json
+import socket
+import logging
+import binascii
+import struct
+import argparse
+import random
+import time
+import math
+
+
+class ServerMessageTypes(object):
+	TEST = 0
+	CREATETANK = 1
+	DESPAWNTANK = 2
+	FIRE = 3
+	TOGGLEFORWARD = 4
+	TOGGLEREVERSE = 5
+	TOGGLELEFT = 6
+	TOGGLERIGHT = 7
+	TOGGLETURRETLEFT = 8
+	TOGGLETURRETRIGHT = 9
+	TURNTURRETTOHEADING = 10
+	TURNTOHEADING = 11
+	MOVEFORWARDDISTANCE = 12
+	MOVEBACKWARSDISTANCE = 13
+	STOPALL = 14
+	STOPTURN = 15
+	STOPMOVE = 16
+	STOPTURRET = 17
+	OBJECTUPDATE = 18
+	HEALTHPICKUP = 19
+	AMMOPICKUP = 20
+	SNITCHPICKUP = 21
+	DESTROYED = 22
+	ENTEREDGOAL = 23
+	KILL = 24
+	SNITCHAPPEARED = 25
+	GAMETIMEUPDATE = 26
+	HITDETECTED = 27
+	SUCCESSFULLHIT = 28
+    
+	strings = {
+		TEST: "TEST",
+		CREATETANK: "CREATETANK",
+		DESPAWNTANK: "DESPAWNTANK",
+		FIRE: "FIRE",
+		TOGGLEFORWARD: "TOGGLEFORWARD",
+		TOGGLEREVERSE: "TOGGLEREVERSE",
+		TOGGLELEFT: "TOGGLELEFT",
+		TOGGLERIGHT: "TOGGLERIGHT",
+		TOGGLETURRETLEFT: "TOGGLETURRETLEFT",
+		TOGGLETURRETRIGHT: "TOGGLETURRENTRIGHT",
+		TURNTURRETTOHEADING: "TURNTURRETTOHEADING",
+		TURNTOHEADING: "TURNTOHEADING",
+		MOVEFORWARDDISTANCE: "MOVEFORWARDDISTANCE",
+		MOVEBACKWARSDISTANCE: "MOVEBACKWARDSDISTANCE",
+		STOPALL: "STOPALL",
+		STOPTURN: "STOPTURN",
+		STOPMOVE: "STOPMOVE",
+		STOPTURRET: "STOPTURRET",
+		OBJECTUPDATE: "OBJECTUPDATE",
+		HEALTHPICKUP: "HEALTHPICKUP",
+		AMMOPICKUP: "AMMOPICKUP",
+		SNITCHPICKUP: "SNITCHPICKUP",
+		DESTROYED: "DESTROYED",
+		ENTEREDGOAL: "ENTEREDGOAL",
+		KILL: "KILL",
+		SNITCHAPPEARED: "SNITCHAPPEARED",
+		GAMETIMEUPDATE: "GAMETIMEUPDATE",
+		HITDETECTED: "HITDETECTED",
+		SUCCESSFULLHIT: "SUCCESSFULLHIT"
+	}
+    
+	def toString(self, id):
+		if id in self.strings.keys():
+			return self.strings[id]
+		else:
+			return "??UNKNOWN??"
+
+
+class ServerComms(object):
+	'''
+	TCP comms handler
+	
+	Server protocol is simple:
+	
+	* 1st byte is the message type - see ServerMessageTypes
+	* 2nd byte is the length in bytes of the payload (so max 255 byte payload)
+	* 3rd byte onwards is the payload encoded in JSON
+	'''
+	ServerSocket = None
+	MessageTypes = ServerMessageTypes()
+	
+	
+	def __init__(self, hostname, port):
+		self.ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.ServerSocket.connect((hostname, port))
+	
+	def readMessage(self):
+		'''
+		Read a message from the server
+		'''
+		messageTypeRaw = self.ServerSocket.recv(1)
+		messageLenRaw = self.ServerSocket.recv(1)
+		messageType = struct.unpack('>B', messageTypeRaw)[0]
+		messageLen = struct.unpack('>B', messageLenRaw)[0]
+		
+		if messageLen == 0:
+			messageData = bytearray()
+			messagePayload = None
+		else:
+			messageData = self.ServerSocket.recv(messageLen)
+			logging.debug("*** {}".format(messageData))
+			messagePayload = json.loads(messageData.decode('utf-8'))
+			
+		logging.debug('Turned message {} into type {} payload {}'.format(
+			binascii.hexlify(messageData),
+			self.MessageTypes.toString(messageType),
+			messagePayload))
+		return messagePayload
+		
+	def sendMessage(self, messageType=None, messagePayload=None):
+		'''
+		Send a message to the server
+		'''
+		message = bytearray()
+		
+		if messageType is not None:
+			message.append(messageType)
+		else:
+			message.append(0)
+		
+		if messagePayload is not None:
+			messageString = json.dumps(messagePayload)
+			message.append(len(messageString))
+			message.extend(str.encode(messageString))
+			    
+		else:
+			message.append(0)
+		
+		logging.debug('Turned message type {} payload {} into {}'.format(
+			self.MessageTypes.toString(messageType),
+			messagePayload,
+			binascii.hexlify(message)))
+		return self.ServerSocket.send(message)
+
+
+# Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
+parser.add_argument('-H', '--hostname', default='127.0.0.1', help='Hostname to connect to')
+parser.add_argument('-p', '--port', default=8052, type=int, help='Port to connect to')
+parser.add_argument('-n', '--name', default='HIVEbot', help='Name of bot')
+args = parser.parse_args()
+
+# Set up console logging
+if args.debug:
+	logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.DEBUG)
+else:
+	logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+
+
+# Connect to game server
+GameServer = ServerComms(args.hostname, args.port)
+
+# Spawn our tank
+logging.info("Creating tank with name '{}'".format(args.name))
+GameServer.sendMessage(ServerMessageTypes.CREATETANK, {'Name': args.name})
+
+
+def getHeading(x1,y1,x2,y2):
+        heading = math.atan2(y1-y2,x1-x2)
+        heading = (heading * (180/math.pi))
+        heading = (heading-360)%360
+        return abs(heading)
+
+def pointTankTo(turn_amount,cur_x,cur_y):
+        offset_ang = getHeading(cur_x,cur_y,0,70)
+        GameServer.sendMessage(ServerMessageTypes.TURNTOHEADING,{"Amount":offset_ang+turn_amount})
+                
+
+def getDistance(x1,y1,x2,y2):
+        heading_x = x2-x1
+        heading_y = y2-y1
+        return(math.sqrt((heading_x*heading_x) + (heading_y*heading_y)))
+
+
+
+def moveTo(x,y):
+        
+        data = GameServer.readMessage()
+        loc_found = False
+        while(not(loc_found)):
+                
+                print(data)
+                if(len(data)>1):
+                        if (data["Name"] == "HIVEbot"):
+                                loc_found=True
+                                cur_y= data["X"]
+                                cur_x = data["Y"]
+                                print("currentx = %f" % cur_x)
+                                print("currenty = %f" % cur_y)
+                                point_to = getHeading(cur_x,cur_y,x,y)
+                                print(point_to)
+                                pointTankTo(point_to,cur_x,cur_y)
+                                distance = getDistance(cur_x,cur_y,x,y)
+                                GameServer.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE, {"Amount":distance})
+                data = GameServer.readMessage()
+                                
+# Main loop - read game messages, ignore them and randomly perform actions
+v=1
+while v:
+        message = GameServer.readMessage()
+        logging.info(message)
+        logging.info("firing")
+        GameServer.sendMessage(ServerMessageTypes.FIRE)
+        #GameServer.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE, {'Amount': 100})
+        #GameServer.sendMessage(ServerMessageTypes.TOGGLELEFT,{'Amount':50})
+        moveTo(0,0)
+        print("HERE")
+        v=0
+
+
+
+
+	
+
+
+    
